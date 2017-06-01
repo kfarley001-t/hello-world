272801131aeabd7b1c0f06bf900b0e6748a7732f
kfarley001-t <kfarley001-t@NCCU.edu>
2017-06-01 11:37:44 -0400

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,11 +1,7 @@
 public class HelloWorld {
-	public static void main(String[args]) {
-		for(int i = 1; i<=10; i++){
-			System.out.println("Hello World, Hello ..." +i);
-		}
-			
-		
-		
-	}
-	
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
+    }
+
 }
